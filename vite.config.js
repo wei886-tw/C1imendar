6ad044f90d5c3a9b90c21209dc804ced3d996ca6
Wei <lunhowl@gmail.com>
2025-05-15 15:39:16 +0800

    [revise] PrivacyView 格式更新

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools()
   ],
-  base: '/C1imendar/'
+  base: '/C1imendar/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
